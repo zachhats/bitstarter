b5dd99d7c179c15f4e0673345eb83fcab8c89b9c
Zach Halasz <halasz2@illinois.edu>
2013-07-13 13:29:48 -0700

    first commit(ish)

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Word!');
+  response.send('Hello Wolrd 2!');
 });
 
 var port = process.env.PORT || 5000;
